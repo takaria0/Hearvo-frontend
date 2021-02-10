dad730a6a381aaf79701da49661447b4bae3052a
takakun <takajcb@gmail.com>
2021-02-10 23:34:51 +0900

    vis

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -8,6 +8,16 @@ import Header from './Header';
 import axios from './Api';
 import { getJwt } from '../helpers/jwt';
 
+const DetailInfo = (props: any) => {
+
+  return (
+    <div style={{  marginTop: 20, borderWidth: 2, borderRadius: 5, backgroundColor: 'white', padding: 5}}>
+      <small><Link to="/tos">利用規約</Link>  <Link to="/privacy">プライバシーポリシー</Link></small>
+  </div>
+  )
+}
+
+
 const SideBar = (props: any) => {
 
   const [popularTopic, setPopularTopic] = useState([]);
@@ -32,17 +42,18 @@ const SideBar = (props: any) => {
       <h3>人気のトピック</h3>
       <div style={{ border: 'none', borderWidth: 2, borderRadius: 5, backgroundColor: 'white'}}>
       
-        <div>
-        {popularTopic.map((elem: any) => {
+        <div style={{padding: 10, paddingTop: 10, paddingBottom: 20}}>
+        {popularTopic.map((elem: any, idx: number) => {
           return (
-          <div style={{paddingLeft: 5, paddingTop: 5}}>
+          <div style={{paddingLeft: 20, paddingTop: 5, marginBottom: 5}}>
               <Link to={`/topic?tp=${elem.topic}`} style={{ textDecoration: 'none', }}>
-                <div>{elem.topic} {elem.num_of_posts}</div>
+                <div>{idx+1} {elem.topic} </div>
               </Link>
           </div>)
         })}
         </div>
       </div>
+      <DetailInfo></DetailInfo>
     </div>
   )
 }
