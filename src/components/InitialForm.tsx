4047aac45139c33fadf63ccd0a5a2a8b7b5fc39e
takakun <takajcb@gmail.com>
2021-04-09 21:05:08 +0900

    fix freeform

--- src/components/InitialForm.tsx
+++ src/components/InitialForm.tsx
@@ -119,7 +119,7 @@ const InitialUserInfoForm = (props: any) => {
               <form className={classes.root} noValidate autoComplete="off">
                 <FormHelperText>
                   <TextField onChange={e => setGenderDetail(e.target.value)} />
-                  {i18n.t("feed.freeform")}
+                  {i18n.t("feed.freeForm")}
                 </FormHelperText></form>
             </div>
             : ''}
